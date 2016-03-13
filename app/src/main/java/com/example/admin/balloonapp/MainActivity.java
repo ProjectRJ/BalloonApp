c73c59f21d6a751085a08d1c23caf75872616d03
jkwok678 <jkwok678@gmail.com>
2016-03-13 23:22:00 +0000

    Importing
    Imported a lot to the Java file and added permission to the Manifest

--- app/src/main/java/com/example/admin/balloonapp/MainActivity.java
+++ app/src/main/java/com/example/admin/balloonapp/MainActivity.java
@@ -2,11 +2,40 @@ package com.example.admin.balloonapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.provider.MediaStore;
-import android.widget.Toast;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
