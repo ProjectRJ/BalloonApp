d6f96b951d4fa32a3e69a7b25feb44a4b070a3c2
jkwok678 <jkwok678@gmail.com>
2016-04-16 15:24:04 +0100

    Starting reorganising

---
+++ app/src/main/java/GPSActivity.java
@@ -0,0 +1,70 @@
+/**
+ * Created by Admin on 16/04/2016.
+ */
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+public class GPSActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+    private static final int Location_request = 0;
+    protected static final String TAG = "location-updates-sample";
+
+    //How long it waits to update. Inexact. Updates may be more or less frequent
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    //The fastest rate for active location updates. Exact.
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    // Keys for storing activity state in the Bundle.
+    protected final static String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+    protected final static String LOCATION_KEY = "location-key";
+    protected final static String LAST_UPDATED_TIME_STRING_KEY = "last-updated-time-string-key";
+
+    //Provides the entry point to Google Play services.
+    protected GoogleApiClient mGoogleApiClient;
+
+    //Stores parameters for requests to the FusedLocationProviderApi.
+    protected LocationRequest mLocationRequest;
+
+    //Represents a geographical location.
+    protected Location mCurrentLocation;
+
+    // UI Widgets.
+    protected Button mStartUpdatesButton;
+    protected Button mStopUpdatesButton;
+    protected TextView mLastUpdateTimeTextView;
+    protected TextView mLatitudeTextView;
+    protected TextView mLongitudeTextView;
+
+    // Labels.
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected String mLastUpdateTimeLabel;
+
+    //Tracks the status of the location updates request. Value changes when the user presses the
+    //Start Updates and Stop Updates buttons.
+    protected Boolean mRequestingLocationUpdates;
+
+    //Time when the location was updated represented as a String.
+    protected String mLastUpdateTime;
+}
